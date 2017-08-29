049b986df9f790690577a69b9145aee8749031d9
tiger4066 <tiger406@qq.com>
2017-08-29 23:11:39 +0800

    add distributed

--- NoticeController.class.php
+++ NoticeController.class.php
@@ -4,11 +4,13 @@ use Think\Controller;
 class NoticeController extends Controller {
 	
 	public function index(){
-		$data = M('notice')->find(I('id'));
+		
 		$result['status'] = 1;
 		$result['msg'] = '获取成功';
 		$result['data'] = $data;
 		exit(json_encode($result));
 	}
 }
-林文彪编辑了
+//林文彪编辑了
+ echo $data
+ wwww
